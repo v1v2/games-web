4ce2c33ce67f04fd99b74fc86b34e5674c6c1343
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2022-03-26 11:10:41 +0700

    chore(tower): use player reference instead of camera rig

--- src/pages/tower.tsx
+++ src/pages/tower.tsx
@@ -1,6 +1,6 @@
 import { Suspense, useEffect, useRef } from 'react'
 
-import { useFrame, useThree } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 import { DefaultXRControllers, Interactive, useXR } from '@codyjasonbennett/xr'
 import { Billboard, Text } from '@react-three/drei'
 import { Object3D } from 'three'
@@ -27,34 +27,27 @@ const PlayerEnv = () => {
   const wave = useMemoryStore(s => s.wave)
 
   const { isPresenting, player, controllers } = useXR()
-  const { camera } = useThree()
 
   const uiRef = useRef<Object3D>(null)
-  const cameraRigRef = useRef<Object3D>(null)
 
   useEffect(() => {
     if (isPresenting) {
-      player.removeFromParent()
-      cameraRigRef.current.add(player)
-      cameraRigRef.current.position.x = 60
-      cameraRigRef.current.position.y = 50
-      cameraRigRef.current.position.z = 60
-      cameraRigRef.current.rotation.y = Math.PI / 4
+      player.position.x = 60
+      player.position.y = 50
+      player.position.z = 60
+      player.rotation.y = Math.PI / 4
       uiRef.current.visible = true
     } else {
-      cameraRigRef.current.position.x = 50
-      cameraRigRef.current.position.y = 50
-      cameraRigRef.current.position.z = 50
-      cameraRigRef.current.rotation.y = 0
+      player.position.x = 50
+      player.position.y = 50
+      player.position.z = 50
+      player.rotation.y = 0
       uiRef.current.visible = false
     }
   }, [isPresenting, JSON.stringify(controllers?.[0]?.controller)])
 
   return (
     <>
-      <group ref={cameraRigRef}>
-        <primitive object={camera} />
-      </group>
       <group ref={uiRef} visible={false} position={[-100, 0, 0]}>
         <Billboard position={[0, 50, 0]} scale={[10, 10, 1]}>
           <Suspense fallback={null}>
@@ -102,7 +95,7 @@ const PlayerEnv = () => {
   )
 }
 
-const IndexPage = () => {
+const TowerPage = () => {
   const isStarted = useMemoryStore(s => s.isStarted)
   const wave = useMemoryStore(s => s.wave)
   const spawnTimerRef = useRef(0)
@@ -166,6 +159,6 @@ const IndexPage = () => {
   )
 }
 
-IndexPage.wrappers = page => <GameLayout>{page}</GameLayout>
+TowerPage.wrappers = page => <GameLayout>{page}</GameLayout>
 
-export default IndexPage
+export default TowerPage
